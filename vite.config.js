802b80e3c16b70f6543d4cae7fe0508db9ed4bdd
Helen Goatly <hgoatly@hotmail.com>
2025-03-22 10:08:21 +0000

    Fix error in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   base: '/helen-manente-yoga/', // Set this to your repository name
+  resolve: {
+    alias: {
+      '@': '/src', // Optional: Add an alias for cleaner imports
+    },
+  },
 });
